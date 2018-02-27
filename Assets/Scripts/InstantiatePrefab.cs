f8e9a77abc38116da73f9d8f8021303eaed0d200
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2018-02-27 00:17:51 -0600

    Build 0.0.1
    Build 0.0.1

---
+++ Assets/Scripts/InstantiatePrefab.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class InstantiatePrefab : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 pos = Vector3.zero;
+        prefab.AddComponent(typeof(Rotate));
+        prefab.GetComponent<Rotate>().RotationAngle = 120;
+        Instantiate(prefab, pos, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private float rotationAngle = 30f ;
+
+    public float RotationAngle
+    {
+        get { return rotationAngle; }
+        set { rotationAngle = value; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationAngle * Time.deltaTime);
+    }
+}
